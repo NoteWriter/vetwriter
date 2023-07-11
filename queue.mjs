70705fc730957df3f947e414ae315f773107fd9f
NoteWriter <boston@getnotewriter.com>
2023-07-11 14:42:20 -0600

    fixed queue.mjs to ES module style

--- queue.mjs
+++ queue.mjs
@@ -1,4 +1,5 @@
 const Queue = require('bull');
 let REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 let workQueue = new Queue('work', REDIS_URL);
-module.exports = workQueue;
+export default workQueue;
+
