ca1a995c018ba48937a1b42c82f72aba733fdb34
NoteWriter <boston@getnotewriter.com>
2023-07-12 09:55:55 -0600

    console.log aws access keys

--- server.mjs
+++ server.mjs
@@ -82,6 +82,8 @@ async function uploadFileToS3(fileBuffer, fileName) {
   });
 }
 
+console.log('AWS_ACCESS_KEY_ID:', process.env.AWS_ACCESS_KEY_ID);
+console.log('AWS_SECRET_ACCESS_KEY:', process.env.AWS_SECRET_ACCESS_KEY);
 
 AWS.config.update({
   region: process.env.BUCKETEER_AWS_REGION,
