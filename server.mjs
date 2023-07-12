c73cfc65755e62cbeb6f4930639fd61700264c62
NoteWriter <boston@getnotewriter.com>
2023-07-12 09:46:22 -0600

    fixed parentheses

--- server.mjs
+++ server.mjs
@@ -88,8 +88,7 @@ AWS.config.update({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
-  };
+  }
 });
 
 const s3 = new AWS.S3();
