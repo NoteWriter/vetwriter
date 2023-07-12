e7ee75352adc52ff685d991588fe94aeb87b19ef
NoteWriter <boston@getnotewriter.com>
2023-07-12 09:41:21 -0600

    v3 style credentials

--- server.mjs
+++ server.mjs
@@ -84,8 +84,12 @@ async function uploadFileToS3(fileBuffer, fileName) {
 
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  region: BUCKETEER_AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+  };
 });
 
 const s3 = new AWS.S3();
