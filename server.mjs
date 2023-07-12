934d598cb912c0531735e435afd3658ab8a9eaa1
NoteWriter <boston@getnotewriter.com>
2023-07-12 09:34:35 -0600

    moved aws.config lower in code

--- server.mjs
+++ server.mjs
@@ -22,16 +22,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || 'http://localhost';
 
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-});
-
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-});
-
 const upload = multer({ storage: multer.memoryStorage() });
 app.use(express.json());
 app.use(cookieParser());
@@ -91,6 +81,14 @@ async function uploadFileToS3(fileBuffer, fileName) {
     });
   });
 }
+
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+const s3 = new AWS.S3();
 
 app.post('/whisper/asr', upload.single('audio'), async (req, res) => {
   const patientName = req.query.patientName; // Extract patient name from query parameters
